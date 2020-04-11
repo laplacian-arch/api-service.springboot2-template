d86438f26a8958252da3765306aae5fe8f12d510
iwauo <iwauo.tajima@gmail.com>
2020-04-12 03:10:31 +0900

    update the project

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "laplacian-arch.template.api-server.springboot"
-pluginManagement {
-    repositories {
-        maven(url = "../mvn-repo/")
-        maven(url = "https://raw.github.com/nabla-squared/mvn-repo/master/")
-        gradlePluginPortal()
-        jcenter()
-    }
-}
