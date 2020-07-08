cd3843b1f772d3a2187d207a939ac96ece47c278
iwauo <iwauo.tajima@gmail.com>
2020-07-09 04:12:17 +0900

    employ reactive database access

--- src/template/dest/{each services as service}{hyphen service.name}/entrypoint.hbs.sh
+++ src/template/dest/{each services as service}{hyphen service.name}/entrypoint.hbs.sh
@@ -7,6 +7,6 @@ java \
   -Djava.security.egd=file:/dev/./urandom \
   ${IMAGE_VERSION:+ -Dimage.version=}${IMAGE_VERSION} \
   -jar /app/api.jar \
-  ${DATASOURCE_URL:+ --spring.datasource.url=}${DATASOURCE_URL} \
-  ${DATASOURCE_USER:+ --spring.datasource.username=}${DATASOURCE_USER} \
-  ${DATASOURCE_PASS:+ --spring.datasource.password=}${DATASOURCE_PASS}
+  ${DATASOURCE_URL:+ --spring.r2dbc.url=}${DATASOURCE_URL} \
+  ${DATASOURCE_USER:+ --spring.r2dbc.username=}${DATASOURCE_USER} \
+  ${DATASOURCE_PASS:+ --spring.r2dbc.password=}${DATASOURCE_PASS}
