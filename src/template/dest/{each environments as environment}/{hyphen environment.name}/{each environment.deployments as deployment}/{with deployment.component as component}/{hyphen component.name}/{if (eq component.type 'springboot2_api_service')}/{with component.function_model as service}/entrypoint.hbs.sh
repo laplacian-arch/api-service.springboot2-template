e71d3f359ee7625e26c2913294de79ffc66f6a2b
iwauo <iwauo.tajima@gmail.com>
2020-09-18 02:22:03 +0900

    update container entrypoint shell

--- src/template/dest/{each environments as environment}/{hyphen environment.name}/{each environment.deployments as deployment}/{with deployment.component as component}/{hyphen component.name}/{if (eq component.type 'springboot2_api_service')}/{with component.function_model as service}/entrypoint.hbs.sh
+++ src/template/dest/{each environments as environment}/{hyphen environment.name}/{each environment.deployments as deployment}/{with deployment.component as component}/{hyphen component.name}/{if (eq component.type 'springboot2_api_service')}/{with component.function_model as service}/entrypoint.hbs.sh
@@ -7,6 +7,24 @@ java \
   -Djava.security.egd=file:/dev/./urandom \
   ${IMAGE_VERSION:+ -Dimage.version=}${IMAGE_VERSION} \
   -jar /app/api.jar \
-  ${DATASOURCE_URL:+ --spring.r2dbc.url=}${DATASOURCE_URL} \
-  ${DATASOURCE_USER:+ --spring.r2dbc.username=}${DATASOURCE_USER} \
-  ${DATASOURCE_PASS:+ --spring.r2dbc.password=}${DATASOURCE_PASS}
+  {{#each service.datasources as |datasource| ~}}
+  {{define "env_prefix"  (concat
+    '${DATASOURCE'
+    (if (eq datasource.name 'default') '' (concat '_' (upper-snake datasource.name)))
+  ) ~}}
+  {{define "prop_prefix" (concat
+    'spring'
+    (if (eq datasource.name 'default') '' (concat '.' (lower-snake datasource.name)))
+    '.r2dbc'
+  ) ~}}
+  {{env_prefix}}_URL:+ --{{prop_prefix}}.url=}{{env_prefix}}_URL} \
+  {{env_prefix}}_USER:+ --{{prop_prefix}}.username=}{{env_prefix}}_USER} \
+  {{env_prefix}}_PASS:+ --{{prop_prefix}}.password=}{{env_prefix}}_PASS} \
+  {{/each}}
+  {{#each service.http_clients as |http_client| ~}}
+  {{define "env_prefix" (concat '${HTTP_CLIENT_' (upper-snake http_client.name)) ~}}
+  {{define "prop_prefix" (concat 'http_client.' (lower-snake http_client.name)) ~}}
+  {{env_prefix}}_BASE_URL:+ --{{prop_prefix}}.base_url=}{{env_prefix}}_BASE_URL} \
+  {{env_prefix}}_API_KEY:+ --{{prop_prefix}}.api_key=}{{env_prefix}}_API_KEY} \
+  {{/each}}
+
