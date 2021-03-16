d8cc803c553030f999836a0d55fb02df9831f52b
iwauo <iwauo.tajima@gmail.com>
2021-03-17 05:13:20 +0900

    split api project into modules allowing to create custom fetchers

--- src/template/scripts/{each environments as environment}/deploy-on-{hyphen environment.name}@additional-declarations@.hbs.sh
+++ src/template/scripts/{each environments as environment}/deploy-on-{hyphen environment.name}@additional-declarations@.hbs.sh
@@ -3,6 +3,14 @@
 {{#if component.custom_built ~}}
 {{#if (eq component.type 'springboot2_api_service_container_image') ~}}
 build_{{lower-snake component.name}}() {
+  (cd $COMPONENT_BASE_DIR/{{hyphen component.name}}-lib
+    ./gradlew publish
+  )
+  {{#if component.function_model.has_custom_logic ~}}
+  (cd $COMPONENT_BASE_DIR/{{hyphen component.name}}-logic
+    ./gradlew publish
+  )
+  {{~/if}}
   (cd $COMPONENT_BASE_DIR/{{hyphen component.name}}
     ./gradlew build
   )
